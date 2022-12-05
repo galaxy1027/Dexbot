61afd391d63425a1f74467f07061c58bcd91a546
Branford Coleman <branfordcoleman@Branfords-MacBook-Pro.local>
2022-12-04 17:31:31 -0800

    Added 'Evolves From' and 'Evolves To' Fields in embed

--- dexbot.py
+++ dexbot.py
@@ -17,13 +17,33 @@ class Client(discord.Client):
 bot = Client()
 tree = app_commands.CommandTree(bot)
 
+
+def make_evolution_chain(species: pokebase.pokemon_species):
+    evolution_chain = [None, None, None]
+    evolution_chain[0] = [species.evolution_chain.chain.species.name]
+
+    if (species.evolution_chain.chain.evolves_to != []):
+        temp_list = []
+        for s in species.evolution_chain.chain.evolves_to:
+            temp_list.append(s.species.name)
+        evolution_chain[1] = temp_list
+
+        if (species.evolution_chain.chain.evolves_to[0].evolves_to != []):
+            temp_list = []
+            for s in species.evolution_chain.chain.evolves_to[0].evolves_to:
+                temp_list.append(s.species.name)
+            evolution_chain[2] = temp_list
+
+    return evolution_chain
+
+
 @tree.command(name = 'search', description = 'Search for a Pokemon')
 async def search(interaction: discord.Interaction, name: str):
     name = name.lower()
     id = pokebase.pokemon_species(name).id
     image = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{id}.png"
     species = pokebase.pokemon_species(name)
-    
+    evolution_chain = make_evolution_chain(species)
     entry = ""
     entry_list = species.flavor_text_entries
     i = len(entry_list) - 1
@@ -40,7 +60,32 @@ async def search(interaction: discord.Interaction, name: str):
     color = discord.Color.blue()
     )
     embed.set_image(url = image)
-    
+
+
+
+    if species.name in evolution_chain[0]:
+        embed.add_field(name = "Evolves From", value = "N/A", inline = True)
+        evolves_to = ""
+        if evolution_chain[1] != None:
+            for s in evolution_chain[1]:
+                evolves_to += s
+                evolves_to += ', '
+            embed.add_field(name = "Evolves To", value = evolves_to, inline = True)
+        else:
+            embed.add_field(name = "Evolves To", value = "N/A", inline = True)
+    elif species.name in evolution_chain[1]:
+        embed.add_field(name = "Evolves From", value = evolution_chain[0][0], inline = True)
+        if evolution_chain[2] != None:
+            evolves_to = ""
+            for s in evolution_chain[2]:
+                evolves_to += s
+                evolves_to += ', '
+            embed.add_field(name = "Evolves To", value = evolves_to, inline = True)
+        else:
+            embed.add_field(name = "Evolves To", value = "N/A", inline = True)
+    else:
+        embed.add_field(name = "Evolves From", value = evolution_chain[1][0], inline = True)
+        embed.add_field(name = "Evolves To", value = "N/A", inline = True)
 
     await interaction.response.send_message(embed=embed)
 
